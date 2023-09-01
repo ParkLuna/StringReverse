588af2b88f8726ff2c0368fac8b4f14bdf40310c
ParkLuna <141165285+ParkLuna@users.noreply.github.com>
2023-09-01 14:06:55 +0530

    Add files via upload

---
+++ String.py
@@ -0,0 +1,6 @@
+def reverse_string(input_string):
+    reversed_string = input_string[::-1]
+    return reversed_string
+my_string = "1234abcd"
+reversed_result = reverse_string(my_string)
+print(reversed_result) 
